9f55ed4af7caa1af2184fa1a0b383117b8665bb8
Ayush Rai <ayushayushravi@gmail.com>
2022-05-19 22:36:03 +0530

    secondcommit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'rai.ayush2610@gmail.com';
 
 
 if($_POST) {
@@ -48,9 +48,11 @@ if($_POST) {
 
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+	  echo ($mail)
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+
+      		// else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
 
